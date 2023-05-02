42abf72f8361c99b0c483b2ac619f5d688596241
Yusuf Göktaş <yusufgk125@gmail.com>
2023-05-02 11:03:37 +0300

    admin
    admin

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  isSidebarOpen = true;
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+    const button = document.querySelector('.aside_button');
+    const main = document.querySelector('.main')
+    if (!this.isSidebarOpen) 
+    {
+      button.classList.add('open');
+      main.classList.add('show_main');
+    } 
+    else 
+    {
+      button.classList.remove('open');
+      main.classList.remove('show_main');
+    }
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth <= 768) {
+      this.isSidebarOpen = false;
+    } else {
+      this.isSidebarOpen = true;
+    }
+  }
 }
