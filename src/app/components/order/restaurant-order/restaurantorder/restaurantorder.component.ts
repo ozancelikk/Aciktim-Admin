f4eae7815b01722aaf66843376fc395b7fdf44c1
yunusarslans <yunusemrearslan76@hotmail.com>
2023-05-03 16:51:44 +0300

    admin-order

---
+++ src/app/components/order/restaurant-order/restaurantorder/restaurantorder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurantorder',
+  templateUrl: './restaurantorder.component.html',
+  styleUrls: ['./restaurantorder.component.css']
+})
+export class RestaurantorderComponent {
+
+}
+
